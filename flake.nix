28fcaebd8598558101315bb9fd57ec97100f4f6e
Artturin <Artturin@artturin.com>
2021-12-30 03:50:48 +0200

    flake: move python overrides upstream

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+      url = "github:artturin/poetry2nix/addoverrides1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils = { url = "github:numtide/flake-utils"; };
@@ -22,23 +22,6 @@
         projectDir = ./.;
         overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
           # Python dependency overrides go here
-          pyudev = super.pyudev.overridePythonAttrs (oldAttrs: {
-            postPatch = ''
-              substituteInPlace src/pyudev/_ctypeslib/utils.py \
-                --replace "find_library(name)" "'${super.pkgs.lib.getLib super.pkgs.systemd}/lib/libudev.so'"
-            '';
-          });
-          icecream = super.icecream.overridePythonAttrs (oldAttrs: {
-            #  # i don't know why the thing below is required when the package in nixpkgs works fine
-            #  # ERROR: Could not find a version that satisfies the requirement executing>=0.3.1 (from icecream) (from versions: none)
-            #  # ERROR: No matching distribution found for executing>=0.3.1
-            postPatch = ''
-              substituteInPlace setup.py --replace 'executing>=0.3.1' 'executing<0.3.1'
-            '';
-          });
-          typing-extensions = super.typing-extensions.overridePythonAttrs (oldAttrs: {
-            format = "flit";
-          });
         });
 
         packageName = "nixos-gen-config";
