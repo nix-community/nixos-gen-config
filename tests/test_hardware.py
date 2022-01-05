122e76e659861d63942704649f425fc07b39dd21
Artturin <Artturin@artturin.com>
2022-01-05 05:24:16 +0200

    tests: test pyudev

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -1,8 +1,31 @@
 import subprocess
+import pytest
+import pyudev
 from unittest.mock import MagicMock, patch
 
 from nixos_gen_config import hardware
 from nixos_gen_config.classes import NixConfigAttrs
+
+
+def test_bcache() -> None:
+    class Device(object):
+        #product = None
+        #device_node = None
+        #action = None
+        #ID_VENDOR_ID = None
+        #ID_MODEL_ID = None
+        ID_FS_TYPE: str
+
+        def get(self, attribute: str) -> pyudev.Device:
+            return getattr(self, attribute)
+
+    pyudev_device = Device()
+    pyudev_device.ID_FS_TYPE = "bcache"
+    nix_config = NixConfigAttrs()
+
+    hardware.bcache(nix_config, pyudev_device)
+
+    assert 'bcache' in nix_config.initrd_available_kernel_modules
 
 
 def test_virt_section() -> None:
